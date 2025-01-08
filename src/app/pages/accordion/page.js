7ce4b474b2cb9981a1abb63a7cccd2dddadc2287
vimal kumar <rootvivek@gmail.com>
2025-01-08 18:57:55 +0530

    again new

--- src/app/pages/accordion/page.js
+++ src/app/pages/accordion/page.js
@@ -5,7 +5,7 @@ import Layout from '../../components/Layout';
 export default function About() {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <h1>Accordion Page</h1>
       <p>This is the about page content.</p>
     </Layout>
   );
--- src/app/pages/button/page.js
+++ src/app/pages/button/page.js
@@ -5,7 +5,7 @@ import Layout from '../../components/Layout';
 export default function About() {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <h1>button Page</h1>
       <p>This is the about page content.</p>
     </Layout>
   );
